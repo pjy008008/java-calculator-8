900f18acd3098434c4b6f7b37163921d02464de2
Junyou Park <pjy008008@gmail.com>
2025-10-19 05:55:47 +0900

    feat(Calculator): 문자열 덧셈 계산기 클래스 구현
    - 다음 기능을 수행하는 add 함수 구현
      - 기본 구분자(, :)를 기준으로 문자열을 분리하여 합산
      - 커스텀 구분자(//{구분자}\n 형식)를 인식하여 동적으로 처리
      - 커스텀 구분자가 숫자이거나 형식이 잘못된 경우 IllegalArgumentException 발생
    
    - 다음 기능을 수행하는 sumTokens 함수 구현
      - 분리된 문자열 토큰을 정수로 변환 후 합산
      - 숫자가 음수일 경우 예외 발생
      - 숫자가 아닌 값이 포함된 경우 예외 발생

---
+++ src/main/java/calculator/domain/Calculator.java
@@ -0,0 +1,52 @@
+package calculator.domain;
+
+public class Calculator {
+    private static final String DEFAULT_DELIMITERS = ",|:";
+    public int add(String input) {
+
+        String numbersPart = input;
+        String delimiters = DEFAULT_DELIMITERS;
+
+        if (input.startsWith("//")) {
+            int newlineIndex = input.indexOf("\\n");
+            if (newlineIndex == -1) {
+                throw new IllegalArgumentException("커스텀 구분자 형식 오류: `\\n`이 들어가야 합니다.");
+            }
+
+            String delimiterPart = input.substring(2, newlineIndex);
+
+            if (delimiterPart.length() != 1) {
+                throw new IllegalArgumentException("커스텀 구분자는 한 글자여야 합니다.");
+            }
+            char customDelimiter = delimiterPart.charAt(0);
+
+            if (Character.isDigit(customDelimiter)) {
+                throw new IllegalArgumentException("커스텀 구분자는 숫자일 수 없습니다.");
+            }
+
+            numbersPart = input.substring(newlineIndex + 2);
+            delimiters = delimiters + "|" + customDelimiter;
+        }
+
+        String[] numberTokens = numbersPart.split(delimiters);
+
+        return sumTokens(numberTokens);
+    }
+
+    private int sumTokens(String[] tokens) {
+        int sum = 0;
+        for (String token : tokens) {
+            try {
+                int number = Integer.parseInt(token);
+
+                if (number < 0) {
+                    throw new IllegalArgumentException("음수는 포함될 수 없습니다.");
+                }
+                sum += number;
+            } catch (NumberFormatException e) {
+                throw new IllegalArgumentException("숫자가 아닌 값이 포함되어 있습니다.");
+            }
+        }
+        return sum;
+    }
+}
