53a8aaa135f69f3f15c19724bf3be8e3aafcac3d
Junyou Park <pjy008008@gmail.com>
2025-10-19 05:57:28 +0900

    feat(OutputView): 계산 결과 및 메시지를 출력하는 뷰 클래스 구현
    - 사용자에게 입력을 요청하는 안내 문구를 출력하는 printPrompt 함수 구현
    - 계산 결과를 출력하는 printResult 함수 구현
    - 예외 발생 시 오류 메시지를 [오류] 형식으로 출력하는 printError 함수 구현

---
+++ src/main/java/calculator/ui/OutputView.java
@@ -0,0 +1,15 @@
+package calculator.ui;
+
+public class OutputView {
+    public void printPrompt() {
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+    }
+
+    public void printResult(int result) {
+        System.out.println("결과 : " + result);
+    }
+
+    public void printError(String message) {
+        System.out.println("[오류] " + message);
+    }
+}
